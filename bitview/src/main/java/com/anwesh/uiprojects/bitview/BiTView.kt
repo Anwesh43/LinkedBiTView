83e94a3755b1f0e9607d3e8a8582c00af121f95b
Anwesh43 <anweshthecool0@gmail.com>
2019-04-11 09:45:00 +0530

    adding libraries and importing modules

--- bitview/src/main/java/com/anwesh/uiprojects/bitview/BiTView.kt
+++ bitview/src/main/java/com/anwesh/uiprojects/bitview/BiTView.kt
@@ -4,3 +4,20 @@ package com.anwesh.uiprojects.bitview
  * Created by anweshmishra on 11/04/19.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.graphics.Color
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
+
