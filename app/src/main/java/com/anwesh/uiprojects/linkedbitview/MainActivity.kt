826d81daefe2bea2195a388abfd1c143ecdbc5e5
Anwesh43 <anweshthecool0@gmail.com>
2019-04-11 10:41:59 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedbitview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbitview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedbitview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.bitview.BiTView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BiTView.create(this)
     }
 }
